9112d852eb1afcb52129bf2fa4ca17b510d4d94e
Gowthamap <gowthamap55555@gmail.com>
2022-01-05 21:32:01 +0530

    todo application started

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const card = {
+    width: "32%",
+    height: "100px",
+    backgroundColor: "green",
+
+}
+
+function Card() {
+    return (
+        <div style={card}>
+            
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/Todos.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const btn = {
+  color: "white",
+  backgroundColor: "green",
+  height: "30px",
+  width: "20%",
+  marginLeft: "5px",
+  fontSize: "16px",
+  fontWeight: "bold"
+};
+
+function Todos() {
+  return (
+    <div style={{paddingBottom:"4%"}}>
+      <h3 style={{marginTop: "2%", marginBottom: "2%"}}>Add Todo</h3>
+      <input type="text" style={{ width: "60%", height: "30px" }} />
+      <button style={btn}>Submit</button>
+    </div>
+  );
+}
+
+export default Todos;
